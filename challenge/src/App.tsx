7fc73c3e3c1c21f9202f6d58c040a1ab901bea94
Thiago da Paz <thiagopazjunior@gmail.com>
2023-05-29 16:12:58 -0300

    feat: Started project.

---
+++ challenge/src/App.tsx
@@ -0,0 +1,14 @@
+// Style and images
+import "./styles/global.css"
+import logoImg from "./assets/logo.svg"
+
+
+function App() {
+  return (
+    <div>
+      Iniciando
+      <img src={logoImg} alt="Logo" />
+    </div>
+  )
+}
+export default App
